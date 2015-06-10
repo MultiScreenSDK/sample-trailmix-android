d8360c03fab80950b3e940fc198d4baa7add7700
Patrick Lin <patrick.lin@samsung.com>
2015-06-10 09:39:27 -0700

    * More UI (wip)

--- app/src/main/java/com/samsung/trailmix/ui/VideoActivity.java
+++ app/src/main/java/com/samsung/trailmix/ui/VideoActivity.java
@@ -2,64 +2,65 @@
 
 package com.samsung.trailmix.ui;
 
-import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.KeyEvent;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.samsung.trailmix.R;
 import com.samsung.trailmix.interceptor.AppCompatActivityMenuKeyInterceptor;
 
 
 /**
- * Initial activity for the application.
+ * Activity for view videos
  */
-public class MainActivity extends AppCompatActivity {
+public class VideoActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.video_activity);
 
         //Initialize the interceptor
         AppCompatActivityMenuKeyInterceptor.intercept(this);
 
         //Add toolbar
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        toolbar.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+        toolbar.setBackgroundColor(Color.GRAY);
+        TextView textView = (TextView)toolbar.findViewById(R.id.appText);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+        textView.setTypeface(Typeface.create("sans-serif-light", Typeface.NORMAL));
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_MENU) {
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
+    public void onConfigurationChanged(final Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        Log.d(this.getClass().getName(), "onCreateOptionsMenu");
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(final MenuItem item) {
-        Log.d(this.getClass().getName(), "onOptionsItemSelected: " + item.toString());
         switch (item.getItemId()) {
             case R.id.action_connect:
                 //When the S icon is clicked, opens the service list dialog.
 //                showServiceListDialog();
-
-                // TODO: Replace with real logic
-                startActivity(new Intent(this, VideoActivity.class));
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
