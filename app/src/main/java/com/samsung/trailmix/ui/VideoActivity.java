a645ce94c5ee902549e7c0c53f4faef35a2055f7
Bin Liu <bliu@samsung.com>
2015-06-11 10:35:54 -0700

    Add video player and related code.

--- app/src/main/java/com/samsung/trailmix/ui/VideoActivity.java
+++ app/src/main/java/com/samsung/trailmix/ui/VideoActivity.java
@@ -5,24 +5,74 @@ package com.samsung.trailmix.ui;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.MediaController;
 import android.widget.TextView;
 
+import com.google.android.exoplayer.VideoSurfaceView;
+import com.google.android.exoplayer.audio.AudioCapabilities;
+import com.google.android.exoplayer.audio.AudioCapabilitiesReceiver;
+import com.google.android.exoplayer.demo.player.DashRendererBuilder;
+import com.google.android.exoplayer.demo.player.DemoPlayer;
+import com.google.android.exoplayer.demo.player.ExtractorRendererBuilder;
+import com.google.android.exoplayer.demo.player.HlsRendererBuilder;
+import com.google.android.exoplayer.demo.player.SmoothStreamingRendererBuilder;
+import com.google.android.exoplayer.extractor.mp3.Mp3Extractor;
+import com.google.android.exoplayer.extractor.mp4.Mp4Extractor;
+import com.google.android.exoplayer.extractor.ts.AdtsExtractor;
+import com.google.android.exoplayer.extractor.ts.TsExtractor;
+import com.google.android.exoplayer.extractor.webm.WebmExtractor;
+import com.google.android.exoplayer.util.Util;
 import com.samsung.trailmix.R;
 import com.samsung.trailmix.interceptor.AppCompatActivityMenuKeyInterceptor;
+import com.samsung.trailmix.player.DemoUtil;
+import com.samsung.trailmix.player.EventLogger;
+import com.samsung.trailmix.player.SmoothStreamingTestMediaDrmCallback;
+import com.samsung.trailmix.player.WidevineTestMediaDrmCallback;
 
 
 /**
  * Activity for view videos
  */
 public class VideoActivity extends AppCompatActivity {
+    public static final int TYPE_DASH = 0;
+    public static final int TYPE_SS = 1;
+    public static final int TYPE_HLS = 2;
+    public static final int TYPE_MP4 = 3;
+    public static final int TYPE_MP3 = 4;
+    public static final int TYPE_M4A = 5;
+    public static final int TYPE_WEBM = 6;
+    public static final int TYPE_TS = 7;
+    public static final int TYPE_AAC = 8;
+
 
     private Toolbar toolbar;
+
+    private EventLogger eventLogger;
+    private MediaController mediaController;
+    private VideoSurfaceView surfaceView;
+
+    private DemoPlayer player;
+    private boolean playerNeedsPrepare;
+
+    private long playerPosition;
+    private boolean enableBackgroundAudio;
+
+    private Uri contentUri;
+    private int contentType;
+    private String contentId;
+
+    private AudioCapabilitiesReceiver audioCapabilitiesReceiver;
+    private AudioCapabilities audioCapabilities;
+
+    //The view to render debug info.
+    private TextView debugTextView = null;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -64,6 +114,100 @@ public class VideoActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // OnClickListener methods
+
+//    @Override
+//    public void onClick(View view) {
+//        if (view == retryButton) {
+//            preparePlayer();
+//        }
+//    }
+
+    // AudioCapabilitiesReceiver.Listener methods
+
+//    @Override
+    public void onAudioCapabilitiesChanged(AudioCapabilities audioCapabilities) {
+        boolean audioCapabilitiesChanged = !audioCapabilities.equals(this.audioCapabilities);
+        if (player == null || audioCapabilitiesChanged) {
+            this.audioCapabilities = audioCapabilities;
+            releasePlayer();
+            preparePlayer();
+        } else if (player != null) {
+            player.setBackgrounded(false);
+        }
+    }
+
+    // Internal methods
+
+
+    private DemoPlayer.RendererBuilder getRendererBuilder() {
+        String userAgent = Util.getUserAgent(this, "ExoPlayerDemo");
+        switch (contentType) {
+            case DemoUtil.TYPE_SS:
+                return new SmoothStreamingRendererBuilder(this, userAgent, contentUri.toString(),
+                        new SmoothStreamingTestMediaDrmCallback(), debugTextView);
+            case DemoUtil.TYPE_DASH:
+                return new DashRendererBuilder(this, userAgent, contentUri.toString(),
+                        new WidevineTestMediaDrmCallback(contentId), debugTextView, audioCapabilities);
+            case DemoUtil.TYPE_HLS:
+                return new HlsRendererBuilder(this, userAgent, contentUri.toString(), debugTextView,
+                        audioCapabilities);
+            case DemoUtil.TYPE_M4A: // There are no file format differences between M4A and MP4.
+            case DemoUtil.TYPE_MP4:
+                return new ExtractorRendererBuilder(this, userAgent, contentUri, debugTextView,
+                        new Mp4Extractor());
+            case DemoUtil.TYPE_MP3:
+                return new ExtractorRendererBuilder(this, userAgent, contentUri, debugTextView,
+                        new Mp3Extractor());
+            case DemoUtil.TYPE_TS:
+                return new ExtractorRendererBuilder(this, userAgent, contentUri, debugTextView,
+                        new TsExtractor(0, audioCapabilities));
+            case DemoUtil.TYPE_AAC:
+                return new ExtractorRendererBuilder(this, userAgent, contentUri, debugTextView,
+                        new AdtsExtractor());
+            case DemoUtil.TYPE_WEBM:
+                return new ExtractorRendererBuilder(this, userAgent, contentUri, debugTextView,
+                        new WebmExtractor());
+            default:
+                throw new IllegalStateException("Unsupported type: " + contentType);
+        }
+    }
+
+    private void preparePlayer() {
+        if (player == null) {
+            player = new DemoPlayer(getRendererBuilder());
+//            player.addListener(this);
+//            player.setTextListener(this);
+//            player.setMetadataListener(this);
+            player.seekTo(playerPosition);
+            playerNeedsPrepare = true;
+            mediaController.setMediaPlayer(player.getPlayerControl());
+            mediaController.setEnabled(true);
+            eventLogger = new EventLogger();
+            eventLogger.startSession();
+            player.addListener(eventLogger);
+            player.setInfoListener(eventLogger);
+            player.setInternalErrorListener(eventLogger);
+        }
+        if (playerNeedsPrepare) {
+            player.prepare();
+            playerNeedsPrepare = false;
+//            updateButtonVisibilities();
+        }
+        player.setSurface(surfaceView.getHolder().getSurface());
+        player.setPlayWhenReady(true);
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playerPosition = player.getCurrentPosition();
+            player.release();
+            player = null;
+            eventLogger.endSession();
+            eventLogger = null;
         }
     }
 
